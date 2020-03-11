15a04aaf0e2c5d438e7c2c05fa97d759c51d8a21
Tobias Kalcher <tkalcher@schule.intern>
2020-03-11 15:25:47 +0100

    üben!!!

--- Tierversuche/src/app/home/home.page.ts
+++ Tierversuche/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  animals: any[] = [];
+  animalForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public formBuilder: FormBuilder) {
+    this.animalForm = this.formBuilder.group({
+    Animal: ['',
+                Validators.compose(
+                  [
+                    Validators.required,
+                    Validators.minLength(2)
+                  ]
+                )],
+    userAnimal: ['',
+                Validators.compose(
+                  [
+                    Validators.required,
+                    Validators.minLength(2)
+                  ]
+                )]
+  });
+}
+
+newAnimal(){
+  console.log(this.animalForm.value);
+
+this.animals.push({
+  userAnimal: this.animalForm.value.userAnimalName
+})
+
+
+  this.animalForm.reset();
+}
 
 }
